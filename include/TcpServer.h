c89d206b7b31385f3fa9d080bd9b8a4ee0dcbfbb
huanyuxun <1320284412@qq.com>
2026-09-19 13:36:58 +0000

    refactor:重构TcpServer结构，逐步socket

--- include/TcpServer.h
+++ include/TcpServer.h
@@ -1,5 +1,4 @@
-#ifndef TCPSERVER_H
-#define TCPSERVER_H
+#pragma once
 
 class TcpServer {
 public:
@@ -8,14 +7,13 @@ public:
     void start();
 
 private:
-    void initServer();
-    int acceptClient();
-    void handleClient(int connfd_);
+    void socket_();//创建socket
+    void bind_();//命名socket，绑定地址
+    void listen_();//监听socket
+    int accept_();//接受连接
+    void handle_(int connfd);
 
 private:
     int port_;
-    int listenfd_;
-    bool isRunning_;
+    int socketfd_;
 };
-
-#endif
--- src/TcpServer.cpp
+++ src/TcpServer.cpp
@@ -5,48 +5,75 @@
 #include <unistd.h>
 #include <cstring>
 
-TcpServer::TcpServer(int port) : port_(port), listenfd_(-1), isRunning_(false) {}
+TcpServer::TcpServer(int port) : port_(port), socketfd_(-1) {}
 
 TcpServer::~TcpServer() {
-    if (listenfd_ >= 0) {
-        close(listenfd_);
-    }
+    if (socketfd_ >= 0) close(socketfd_);
 }
 
 void TcpServer::start() {
-    initServer(); 
-    while (isRunning_) {
-        std::cout << "Waiting for client..." << std::endl;
-        int connfd = acceptClient(); 
-        if (connfd < 0) {
-            continue;
-        }
-        handleClient(connfd); 
+    socket_();
+    bind_();
+    listen_();
+    while (true) {
+        int connfd = accept_();
+        handle_(connfd);
     }
-    close(listenfd_);
 }
 
-void TcpServer::initServer() {
-    listenfd_ = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    addr.sin_port = htons(port_); 
-    bind(listenfd_, (sockaddr*)&addr, sizeof(addr));
-    listen(listenfd_, 5);
-    isRunning_ = true;
-    std::cout << "Server initialized on port " << port_ << std::endl;
+//创建一个标准的IPv4 Tcp socket
+void TcpServer::socket_() {
+    socketfd_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd_ == -1) {
+        //logger:socket err
+        std::cout << "socket创建失败：" << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    //logger:socket suc
 }
 
-int TcpServer::acceptClient() {
-    sockaddr_in clientAddr;
-    socklen_t clientLen = sizeof(clientAddr);
-    int connfd = accept(listenfd_, (sockaddr*)&clientAddr, &clientLen);
+//绑定
+void TcpServer::bind_() {
+    struct sockaddr_in my_addr;
+    memset(&my_addr, 0, sizeof(my_addr)); 
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(port_);
+    my_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    int ret = bind(socketfd_, (struct sockaddr*)&my_addr, sizeof(my_addr));
+    if (ret == -1) {
+        //logger:bind err
+        std::cout << "bind绑定失败：" << strerror(errno) << std::endl;// 如果出错，这里就会捕获到 EACCES 或 EADDRINUSE
+        exit(EXIT_FAILURE);
+    }
+    //logger:bind suc
+}
+
+//监听
+void TcpServer::listen_() {
+    int res = listen(socketfd_, 5);
+    if (res == -1) {
+        //logger:listen err
+        std::cout << "listen失败：" << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    //logger:listen suc
+}
+
+//连接
+int TcpServer::accept_() {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    int connfd = accept(socketfd_, (sockaddr*)&client_addr, &client_len);
+    if (connfd == -1) {
+        //logger:accept err
+        std::cout << "accept失败：" << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    //logger:accept suc
     return connfd;
 }
 
-void TcpServer::handleClient(int connfd) {
+void TcpServer::handle_(int connfd) {
     char buf[1024]; 
     while (true) {
         memset(buf, 0, sizeof(buf)); 
